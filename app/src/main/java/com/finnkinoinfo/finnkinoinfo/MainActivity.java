d244dcd16b23bf535dc30493f8a76c4345c6b1f5
Rookit15 <attehiltunen@hotmail.com>
2022-04-05 18:17:50 +0300

    half of mainpage

--- app/src/main/java/com/finnkinoinfo/finnkinoinfo/MainActivity.java
+++ app/src/main/java/com/finnkinoinfo/finnkinoinfo/MainActivity.java
@@ -4,11 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ArrayList<String> names = new ArrayList<String>();
+
     }
 }
