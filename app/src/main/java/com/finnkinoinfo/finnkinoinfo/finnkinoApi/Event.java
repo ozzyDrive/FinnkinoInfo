7b19fdfe48193a3d526d7d9862b5f09a6d78fe22
ozzyDrive <ozzfi1@gmail.com>
2022-05-01 19:58:22 +0300

    Implement Event's getRating function

--- app/src/main/java/com/finnkinoinfo/finnkinoinfo/finnkinoApi/Event.java
+++ app/src/main/java/com/finnkinoinfo/finnkinoinfo/finnkinoApi/Event.java
@@ -1,14 +1,31 @@
 package com.finnkinoinfo.finnkinoinfo.finnkinoApi;
 
+import com.finnkinoinfo.finnkinoinfo.imdbApi.ImdbApiClient;
+
+import java.io.IOException;
+
 public class Event {
     String name;
+    String id;
     int lengthInMinutes;
     int productionYear;
     String description;
     String ageRestriction;
     String thumbnail;
 
-    int getRating() {
-        return 0;
+    ImdbApiClient imdbApiClient;
+
+    public Event(ImdbApiClient imdbApiClient) {
+        this.imdbApiClient = imdbApiClient;
+    }
+
+    /***
+     * Fetches the movie's rating value from ImDb API
+     * @return integer value of rating
+     * @throws IOException
+     */
+    int getRating() throws IOException {
+        assert id != null;
+        return imdbApiClient.getRating(id).imDb;
     }
 }
