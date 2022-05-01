019819079c9cdcb2e09682d3980edf23acf69c86
Rookit15 <attehiltunen@hotmail.com>
2022-05-01 18:18:00 +0300

    Both activities attached

--- app/src/main/java/com/finnkinoinfo/finnkinoinfo/MovieActivity.java
+++ app/src/main/java/com/finnkinoinfo/finnkinoinfo/MovieActivity.java
@@ -2,25 +2,37 @@ package com.finnkinoinfo.finnkinoinfo;
 
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.loader.content.AsyncTaskLoader;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.Drawable;
+import android.media.Image;
+import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.finnkinoinfo.finnkinoinfo.finnkinoApi.Event;
 import com.finnkinoinfo.finnkinoinfo.finnkinoApi.FinnkinoApiClient;
+import com.koushikdutta.ion.Ion;
 
 import org.xml.sax.SAXException;
 
 import java.io.IOException;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
 
 import javax.xml.parsers.ParserConfigurationException;
 
 public class MovieActivity extends AppCompatActivity {
-
+    ImageView pictureView;
     FinnkinoApiClient finnkinoApiClient;
+
     {
         try {
             finnkinoApiClient = new FinnkinoApiClient();
@@ -34,15 +46,19 @@ public class MovieActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movie);
-
+        Bundle extras = getIntent().getExtras();
+        int eventId = extras.getInt("EventID");
+        pictureView = findViewById(R.id.imageView);
         TextView movieName = findViewById(R.id.movie_name_textView);
-
-        int eventID = 303823;
 
         ArrayList<Event> events;
 
+
         try {
-            events = finnkinoApiClient.getSchedule(null,null, java.util.Optional.of(eventID));
+            events = finnkinoApiClient.getSchedule(null, null, java.util.Optional.of(eventId));
+
+
+
         } catch (IOException e) {
             e.printStackTrace();
         } catch (SAXException e) {
